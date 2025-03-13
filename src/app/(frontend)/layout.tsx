5e40bd4100ffcb35f8c2e636832bed8019b7613d
Mike Cebulski <mikecebul@gmail.com>
2025-03-13 19:24:35 -0400

    feat: add Tailwind CSS configuration and implement UI components

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.css'
+import './globals.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -12,7 +12,9 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <main>{children}</main>
+        <main className="flex flex-col contatiner mx-auto h-dvh items-center py-24">
+          {children}
+        </main>
       </body>
     </html>
   )
